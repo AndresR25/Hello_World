71b41f13e21fdc1be39f6c8cde32ff478c816a8c
AndresR25 <andresr25.07@gmail.com>
2021-09-20 13:36:25 -0600

    Hello World

---
+++ hello_world.py
@@ -0,0 +1,21 @@
+print("Hello World1")
+
+name="Andres Rodriguez"
+print("Hello ",name,"!")
+
+print("Hello "+name+" !")
+
+number=7
+
+print("Hello ",number,"!")
+
+print("Hello "+str(number)+" !")
+
+food1="Hamburguer"
+food2="Pizza"
+
+print("I love to eat {} and {}".format(food1,food2))
+
+print(f"I love to eat {food1} and {food2}")
+
+print("I love to eat %d %s" % (2,"pizzas"))
